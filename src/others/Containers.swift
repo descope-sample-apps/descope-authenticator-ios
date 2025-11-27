af7f414082d191c2b41d008308116afa736aafb2
Gil Shapira <gshapira@gmail.com>
2025-11-27 14:03:02 +0200

    Project upgrade

--- src/others/Containers.swift
+++ src/others/Containers.swift
@@ -1,15 +1,8 @@
 
 class WeakCollection<T>: Sequence {
-    
-    typealias Element = T
-
-    func makeIterator() -> IndexingIterator<[T]> {
-        return references.compactMap { $0.element as? T }.makeIterator()
-    }
-
     func add(_ element: AnyObject) {
         references.removeAll { $0.element == nil }
-        references.append(WeakReference(to: element))
+        references.append(WeakReference(element: element))
     }
 
     func remove(_ element: AnyObject) {
@@ -18,15 +11,13 @@ class WeakCollection<T>: Sequence {
         }
     }
 
-    // Internal
-
+    func makeIterator() -> IndexingIterator<[T]> {
+        return references.compactMap { $0.element as? T }.makeIterator()
+    }
+    
     private var references: [WeakReference] = []
 
-    private class WeakReference {
+    private struct WeakReference {
         weak var element: AnyObject?
-
-        init(to element: AnyObject) {
-            self.element = element
-        }
     }
 }
